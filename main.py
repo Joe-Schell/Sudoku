4a050c3724c241c4cf6112785cc88a10c165017e
jschell <josephschell510@gmail.com>
2022-06-19 23:01:09 -0600

    Added an example board and print board function

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ board = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
+
+# Function that prints the board
+# Input: 2D Array (board)
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+print_board(board)
