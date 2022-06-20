4ee39aa6348c2b21b8161971c00dd675c069c495
jschell <josephschell510@gmail.com>
2022-06-20 02:39:08 -0600

    Finished the solve function that solves a board

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ ROWS, COLS = len(board1), len(board1[0])
 
 
 # Function that solves a sudoku board
-# @param: board - a 2D array example board
-# @return:
 def solve(board):
     find = find_empty(board)
     # Base Case: We found no empty cells remaining
@@ -28,39 +26,30 @@ def solve(board):
         row, col = find
     # Loop from 1 - 9 since this is the set of valid numbers in a board
     for i in range(1, 10):
-        if valid(board[row]):
+        # Check for a valid cell in the board
+        if valid(board, row, col, i):
+            # If valid, add i to the board
+            board[row][col] = i
+            # Recursively call solve on the board
+            # and return True as long as the function returns True
+            if solve(board):
+                return True
+            # Backtrack by setting the cell back to 0 (empty)
+            board[row][col] = 0
+    # If we get here, we cannot solve the board and must return False
+    return False
 
 
-# Function that checks if a sudoku board is valid
-# @param: board - a 2D array example board
-# @return: True or False
-def valid(board):
-    # Hashset solution: O(N^2) time | O(N^2) space
-    cols = collections.defaultdict(set)
-    rows = collections.defaultdict(set)
-    squares = collections.defaultdict(set)
+# Function that checks if a sudoku board cell is valid
+def valid(board, row, col, num):
+    not_in_row = num not in board[row]
+    not_in_col = num not in [board[i][col] for i in range(9)]
+    not_in_box = num not in [board[i][j] for i in range(row//3*3, row//3*3+3)
+                             for j in range(col//3*3, col//3*3+3)]
+    return not_in_row and not_in_col and not_in_box
 
-    for r in range(len(board)):
-        for c in range(len(board[0])):
-            # Empty space is found, continue to next iteration
-            if board[r][c] == 0:
-                continue
-
-            # If number in the board is already in the row / col / square, return False
-            if (board[r][c] in rows[r] or board[r][c] in cols[c] or
-                board[r][c] in squares[(r // 3, c // 3)]):
-                return False
-
-            # Update hashsets
-            cols[c].add(board[r][c])
-            rows[r].add(board[r][c])
-            squares[(r // 3, c // 3)].add(board[r][c])
-
-    return True
 
 # Function that prints the board
-# @param: board - a 2D array example board
-# @return: None (prints to console)
 def print_board(board):
     for i in range(ROWS):
         if i % 3 == 0 and i != 0:
@@ -75,8 +64,6 @@ def print_board(board):
 
 
 # Function that finds empty cell in the Sudoku board
-# @param: board - a 2D array example board
-# @return: Empty cell or None
 def find_empty(board):
     for i in range(ROWS):
         for j in range(COLS):
@@ -86,5 +73,6 @@ def find_empty(board):
     return None
 
 print_board(board1)
-var = valid(board1)
-print(var)
+print("solving....")
+solve(board1)
+print_board(board1)
